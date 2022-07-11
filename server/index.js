e7af1858d9ba2eabab9a488b9e1046967e6b8830
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-07-11 23:51:35 +0530

    created server

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from 'dotenv';
+
+
+const app = express();
+dotenv.config();
+
+
+app.get('/', (req, res)=>{
+    res.send("Weclome to my Portfolio");
+})
+
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+    .then(()=>app.listen(PORT, ()=>console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch(err=>console.log(`${err} Error connecting to database`));
