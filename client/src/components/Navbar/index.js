eb0655b1a34ec99e478f02bab7bda3e149c52509
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-07-16 00:07:49 +0530

    Navigation Bar + Avatar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,7 +1,8 @@
-import React from 'react';import { ReactNode } from 'react';
+import React from "react";
+import { ReactNode } from "react";
 import {
-    Text,
-    Badge,
+  Text,
+  Badge,
   Box,
   HStack,
   Flex,
@@ -19,112 +20,188 @@ import {
   Stack,
   useColorMode,
   Center,
-} from '@chakra-ui/react';
-import { MoonIcon, SunIcon,HamburgerIcon,CloseIcon } from '@chakra-ui/icons';
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon, HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
+// const Links = ['Home', 'About', 'Projects', 'Blog', 'Contact'];
 
-const Links = ['Home', 'About', 'Projects', 'Blog', 'Contact'];
-
-const NavLink = ({ children }: { children: ReactNode }) => (
-  <Link
-    px={2}
-    py={1}
-    rounded={'md'}
-    _hover={{
-      textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
-    }}
-    href={'#'}>
-    {children}
-  </Link>
-);
+// const NavLink = ({ children }: { children: ReactNode }) => (
+// <Link
+//   px={2}
+//   py={1}
+//   rounded={'md'}
+//   _hover={{
+//     textDecoration: 'none',
+//     bg: useColorModeValue('gray.200', 'gray.700'),
+//   }}
+//   href='#'>
+//   {children}
+// </Link>
+// );
 
 const Navbar = () => {
-    const { colorMode, toggleColorMode } = useColorMode();
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    return (
-      <>
-        <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-          <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
           <IconButton
-            size={'md'}
+            size={"md"}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
             onClick={isOpen ? onClose : onOpen}
           />
-          <HStack spacing={8} alignItems={'center'}>
-            <Box bgGradient='linear(to-l, #7928CA, #FF0080)'
-                bgClip='text'
-                fontSize='2xl'
-                fontWeight='extrabold'
-                fontFamily='sans-serif'
-                >Yash Kapure</Box>
+          <HStack spacing={8} alignItems={"center"}>
+            <Box
+              bgGradient="linear(to-l, #7928CA, #FF0080)"
+              bgClip="text"
+              fontSize="2xl"
+              fontWeight="extrabold"
+              fontFamily="sans-serif"
+            >
+              Yash Kapure
+            </Box>
             <HStack
-              as={'nav'}
+              as={"nav"}
               spacing={4}
-              display={{ base: 'none', md: 'flex' }}>
-              {Links.map((link) => (
+              display={{ base: "none", md: "flex" }}
+            >
+              {/* {Links.map((link) => (
                 <NavLink key={link}>{link}</NavLink>
-              ))}
+              ))} */}
+              <Link
+                px={2}
+                spacing={4}
+                display={{ base: "none", md: "flex" }}
+                py={1}
+                rounded={"md"}
+                _hover={{
+                  textDecoration: "none",
+                  bg: useColorModeValue("gray.200", "gray.700"),
+                }}
+                href={"#"}
+              >
+                Home
+              </Link>
+              <Link
+                px={2}
+                spacing={4}
+                display={{ base: "none", md: "flex" }}
+                py={1}
+                rounded={"md"}
+                _hover={{
+                  textDecoration: "none",
+                  bg: useColorModeValue("gray.200", "gray.700"),
+                }}
+                href={"#about"}
+              >
+                About
+              </Link>
+              <Link
+                px={2}
+                spacing={4}
+                display={{ base: "none", md: "flex" }}
+                py={1}
+                rounded={"md"}
+                _hover={{
+                  textDecoration: "none",
+                  bg: useColorModeValue("gray.200", "gray.700"),
+                }}
+                href={"#projects"}
+              >
+                Projects
+              </Link>
+              <Link
+                px={2}
+                spacing={4}
+                display={{ base: "none", md: "flex" }}
+                py={1}
+                rounded={"md"}
+                _hover={{
+                  textDecoration: "none",
+                  bg: useColorModeValue("gray.200", "gray.700"),
+                }}
+                href={"#blog"}
+              >
+                Blog
+              </Link>
+              <Link
+                px={2}
+                spacing={4}
+                display={{ base: "none", md: "flex" }}
+                py={1}
+                rounded={"md"}
+                _hover={{
+                  textDecoration: "none",
+                  bg: useColorModeValue("gray.200", "gray.700"),
+                }}
+                href={"#contact"}
+              >
+                Contact
+              </Link>
             </HStack>
-            </HStack>
-            <Flex alignItems={'center'}>
-              <Stack direction={'row'} spacing={7}>
-                <Button onClick={toggleColorMode}>
-                  {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-                </Button>
-  
-                <Menu>
-                  <MenuButton
-                    as={Button}
-                    rounded={'full'}
-                    variant={'link'}
-                    cursor={'pointer'}
-                    minW={0}>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={"full"}
+                  variant={"link"}
+                  cursor={"pointer"}
+                  minW={0}
+                >
+                  <Avatar
+                    size={"sm"}
+                    src={"https://avatars.githubusercontent.com/u/61585443?v=4"}
+                  />
+                </MenuButton>
+                <MenuList alignItems={"center"}>
+                  <br />
+                  <Center>
                     <Avatar
-                      size={'sm'}
-                      src={'https://avatars.githubusercontent.com/u/61585443?v=4'}
+                      size={"2xl"}
+                      src={
+                        "https://avatars.githubusercontent.com/u/61585443?v=4"
+                      }
                     />
-                  </MenuButton>
-                  <MenuList alignItems={'center'}>
-                    <br />
-                    <Center>
-                      <Avatar
-                        size={'2xl'}
-                        src={'https://avatars.githubusercontent.com/u/61585443?v=4'}
-                      />
-                    </Center>
-                    <br />
-                    <Center>
+                  </Center>
+                  <br />
+                  <Center>
                     <Box>
-                    <Text fontWeight='bold'>
-                    Yash Kapure
-                    <Badge ml='1' colorScheme='green'>
-                        New
-                    </Badge>
-                    </Text>
-                    <Text fontSize='sm'> Software Developer</Text>
+                      <Text fontWeight="bold">
+                        Yash Kapure
+                        <Badge ml="1" colorScheme="green">
+                          New
+                        </Badge>
+                      </Text>
+                      <Text fontSize="sm"> Software Developer</Text>
                     </Box>
-                    </Center>
-                    <br />
-                  </MenuList>
-                </Menu>
-              </Stack>
-            </Flex>
+                  </Center>
+                  <br />
+                </MenuList>
+              </Menu>
+            </Stack>
           </Flex>
-          {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }}>
-            <Stack as={'nav'} spacing={4}>
-              {Links.map((link) => (
+        </Flex>
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {/* {Links.map((link) => (
                 <NavLink key={link}>{link}</NavLink>
-              ))}
+              ))} */}
             </Stack>
           </Box>
         ) : null}
-        </Box>
-      </>
-    );
-}
+      </Box>
+    </>
+  );
+};
 
 export default Navbar;
