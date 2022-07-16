03b40065ce0461713358d935bfc883e45d9c8a49
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-07-17 00:31:50 +0530

    changed api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://portfoliomern.herokuapp.com/posts';
+const url = 'https://mernportfolioyash.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
