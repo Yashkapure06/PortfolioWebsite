08bef52199b980279a0d77c2768cbd8bf6bc127b
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-09-01 20:39:20 +0530

    Added function for animation

--- client/src/App.js
+++ client/src/App.js
@@ -9,15 +9,32 @@ import TestimonialMain from './components/Testimonials';
 import About from './components/About';
 import Projects from './components/Projects';
 import Blog from './components/Blog';
-import Comments from './components/Comments';
+// import Comments from './components/Comments';
 
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
 const App=() =>{
     
   return (
       <ChakraProvider>
         <Navbar/>
         <Hero/>
-        <About/>
+        <About />
         <Projects/>
         {/* <Comments/> */}
         <Divider/>
