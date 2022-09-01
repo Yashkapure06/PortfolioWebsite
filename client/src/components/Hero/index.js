2bb447cd0c514b0d88c9a58f57c60f439f56d4a7
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-09-01 20:40:29 +0530

    Added animation part

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -216,25 +216,9 @@ export const Blob = (props: IconProps) => {
         clipRule="evenodd"
         className="blob"
         d="M239.184 439.443c-55.13-5.419-110.241-21.365-151.074-58.767C42.307 338.722-7.478 282.729.938 221.217c8.433-61.644 78.896-91.048 126.871-130.712 34.337-28.388 70.198-51.348 112.004-66.78C282.34 8.024 325.382-3.369 370.518.904c54.019 5.115 112.774 10.886 150.881 49.482 39.916 40.427 49.421 100.753 53.385 157.402 4.13 59.015 11.255 128.44-30.444 170.44-41.383 41.683-111.6 19.106-169.213 30.663-46.68 9.364-88.56 35.21-135.943 30.551z"
-        fill="currentColor"
+        fill="#FF0080"
       />
     </Icon>
   );
 };
 
-
-// const Popup = () => {
-//   return (
-//     <>
-//     <Popover>
-    
-    // <PopoverContent>
-    //   <PopoverArrow />
-    //   <PopoverCloseButton />
-    //   <PopoverHeader>Confirmation!</PopoverHeader>
-    //   <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
-    // </PopoverContent>
-//   </Popover>
-//   </>
-//   );
-// }
