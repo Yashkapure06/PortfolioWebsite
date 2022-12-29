d727650031bd1d7bd51c7b9375dc326b293aaafc
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-12-29 15:26:01 +0530

    Added Contact form

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,160 +1,64 @@
-import React from 'react';
+import React from "react";
+// import contactImg from "./Contact.png";
+import { useForm, ValidationError } from "@formspree/react";
 import {
-    Container,
-    Flex,
-    Box,
-    Heading,
-    Text,
-    IconButton,
-    Button,
-    VStack,
-    HStack,
-    Wrap,
-    WrapItem,
-    FormControl,
-    FormLabel,
-    Input,
-    InputGroup,
-    InputLeftElement,
-    Textarea,
-    Link,
-  } from '@chakra-ui/react';
-  import {
-    MdPhone,
-    MdEmail,
-    MdLocationOn,
-    MdFacebook,
-    MdOutlineEmail,
-  } from 'react-icons/md';
-  import { BsGithub, BsDiscord, BsPerson, BsInstagram, BsLinkedin } from 'react-icons/bs';
-  import Comments from '../Comments/index';
+  Text,
+  Button,
   
+} from "@chakra-ui/react";
+import "./ContactForm.css";
 
 const Contact = () => {
+  const [state, handleSubmit] = useForm("mbjervyr");
+  if (state.succeeded) {
+    return <h3 style={{textAlign: 'center', marginTop: 56, padding:50 }}>Thanks for Connecting!!<br/> We'll be back to you soon.</h3>;
+  }
+  return (
+      <center>
+    <section className="contact-section flex reveal">
+      
+        <div className="contact-form">
+          <Text style={{textAlign: 'center', marginBottom:'20px',fontSize:56}}>Let's  Connect</Text>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <input  className='input' type="text" name="name" placeholder="Name" required />
+              <ValidationError
+                prefix="Name"
+                field="name"
+                errors={state.errors}
+              />
+            </div>
 
-  const ButtonMailto = ({ mailto, label }) => {
-    return (
-        <Link
-            style={{textDecoration:'none'}}
-            _hover={{textDecoration:'none'}}         
-            to='#'
-            onClick={(e) => {
-                window.location.href = mailto;
-                e.preventDefault();
-            }}
-        >
-          <Button mt={4}
-                        variant="solid"
-                        bg="#0D74FF"
-                        color="white"
-                        _hover={{bgGradient:'linear(to-l, #7928CA, #FF0080)'}} >
-                            {label}
-                          </Button>
-        </Link>
-    );
-  };
-    return (
-       <Container className="reveal" id="contact" bgGradient='linear(to-l, #7928CA, #FF0080)'  maxW="full" mt={0} centerContent overflow="hidden" isLazy>
-      <Flex
-      >
-        <Box
-          bg="#02054B"
-          color="white"
-          borderRadius="lg"
-          p={{ sm: 5, md: 5, lg: 6 }}
-          m={{ sm: 4, md: 16, lg: 25 }}
-          width="100%"
-          >
-          <Box p={4}>
-            <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
-              <WrapItem>
-                <Box>
-                  <Heading>Contact</Heading>
-                  <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.500">
-                    Fill up the form below to contact
-                  </Text>
-                  <Box py={{ base: 5, sm: 8, md: 8, lg: 10 }}>
-                    <VStack pl={0} spacing={3} alignItems="flex-start">
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="200px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: '2px solid #1C6FEB' }}
-                        leftIcon={<MdPhone color="#1970F1" size="20px" />}>
-                        <Link href="tel:7028073007">+91-7028073007</Link>
-                      </Button>
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="255px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: '2px solid #1C6FEB' }}
-                        leftIcon={<MdEmail color="#1970F1" size="20px" />}>
-                       <Link href="mailto:yashkapure06@gmail.com"> yashkapure06@gmail.com</Link>
-                      </Button>
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="200px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: '2px solid #1C6FEB' }}
-                        leftIcon={<MdLocationOn color="#1970F1" size="20px" />}>
-                        Maharashtra, India
-                      </Button>
-                    </VStack>
-                  </Box>
-                  <HStack
-                    mt={{ lg: 10, md: 10 }}
-                    spacing={5}
-                    px={5}
-                    alignItems="flex-start">
-                    <IconButton
-                      aria-label="facebook"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{bgGradient:'linear(to-l, #7928CA, #FF0080)'  }}
-                      icon={<BsInstagram size="28px" />}
-                      onClick={() => {
-                        window.open("https://www.instagram.com/_yashkapure_/", "_blank");
-                      }}
-                    />
-                    <IconButton
-                      aria-label="github"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{ bgGradient:'linear(to-l, #7928CA, #FF0080)'  }}
-                      icon={<BsGithub size="28px" />}
-                      onClick={() => {
-                        window.open("https://github.com/Yashkapure06", "_blank");
-                      }}
-                    />
-                    <IconButton
-                      aria-label="discord"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{ bgGradient:'linear(to-l, #7928CA, #FF0080)'  }}
-                      icon={<BsLinkedin size="28px" />}
-                      onClick={() => {
-                        window.open("https://www.linkedin.com/in/yash-kapure-9090a01a9/", "_blank");
-                      }}
-                    />
-                  </HStack>
-                </Box>
-              </WrapItem>
-            </Wrap>
-          </Box>
-        </Box>
-      </Flex>
-              {/* <Comments /> */}
-    </Container>
-    );
-}
+            <div>
+              <input className="input" type="email" name="email" placeholder="Email" required />
+            </div>
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+            <div>
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Message"
+                row="30"
+                cols="50"
+              ></textarea>
+              <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+              />
+            </div>
+            <Button type="submit" bgGradient="linear(to-l, #7928CA, #FF0080)" disabled={state.submitting}>
+              Send
+            </Button>
+          </form>
+        </div>
+    </section>
+      </center>
+  );
+};
 
 export default Contact;
