42655ac8edaf73f2908e2aa299c5f8deb0b4b378
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-05-18 12:03:27 +0530

    Updated some minor changes

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -28,6 +28,13 @@ import {
   // TabPanel,
 } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faTwitter,
+  faLinkedin,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 import heroImage from "./image/DSC_0094.JPG";
 import resume from "./image/resume.png";
@@ -100,8 +107,8 @@ const Hero = () => {
                 query="React Native 📱"
                 styles={{ px: "2", py: "0.3", rounded: "full", bg: "blue.100" }}
               >
-                I am currently expanding my skillset to include React Native 📱 for
-                mobile development.
+                I am currently expanding my skillset to include React Native 📱
+                for mobile development.
               </Highlight>
             </Text>
             <Stack
@@ -197,21 +204,37 @@ const Hero = () => {
                 h={"100%"}
                 src={heroImage}
               />
+              
             </Box>
           </Flex>
         </Stack>
+        <Stack
+          spacing={{ base: 4, sm: 6 }}
+          direction={{ base: "column", sm: "row" }}
+          align="center" // Align the text and icons horizontally
+        >
+          <Text fontSize={{ base: "3xl", sm: "2xl", lg: "3xl" }}>Connect with me :</Text>
+          <a href="https://github.com/Yashkapure06" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+          <a href="https://linkedin.com/in/yash-kapure" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+          <a href="https://www.instagram.com/_yashkapure_" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </a>
+          <a href="https://twitter.com/KapureYash" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </a>
+          
+        </Stack>
+
       </Container>
     </>
   );
 };
 
 export default Hero;
-
-const PlayIcon = createIcon({
-  displayName: "PlayIcon",
-  viewBox: "0 0 58 58",
-  d: "M28.9999 0.562988C13.3196 0.562988 0.562378 13.3202 0.562378 29.0005C0.562378 44.6808 13.3196 57.438 28.9999 57.438C44.6801 57.438 57.4374 44.6808 57.4374 29.0005C57.4374 13.3202 44.6801 0.562988 28.9999 0.562988ZM39.2223 30.272L23.5749 39.7247C23.3506 39.8591 23.0946 39.9314 22.8332 39.9342C22.5717 39.9369 22.3142 39.8701 22.0871 39.7406C21.86 39.611 21.6715 39.4234 21.5408 39.1969C21.4102 38.9705 21.3421 38.7133 21.3436 38.4519V19.5491C21.3421 19.2877 21.4102 19.0305 21.5408 18.8041C21.6715 18.5776 21.86 18.3899 22.0871 18.2604C22.3142 18.1308 22.5717 18.064 22.8332 18.0668C23.0946 18.0696 23.3506 18.1419 23.5749 18.2763L39.2223 27.729C39.4404 27.8619 39.6207 28.0486 39.7458 28.2713C39.8709 28.494 39.9366 28.7451 39.9366 29.0005C39.9366 29.2559 39.8709 29.507 39.7458 29.7297C39.6207 29.9523 39.4404 30.1391 39.2223 30.272Z",
-});
 
 export const Blob = (props: IconProps) => {
   return (
